19945637e2b4cc459a4388e43459d95fbf78ff03
Abiel Hailemariam <abielh06@gmail.com>
2020-01-21 21:26:52 -0600

    database created

---
+++ models/game.js
@@ -0,0 +1,47 @@
+module.exports = function(sequelize, DataTypes) {
+  var Game = sequelize.define("Game", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    game_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    game_type: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    played: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    wishlist: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }, 
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+
+  });
+  return Game;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,23 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/games", function(req, res) {
+    db.Game.findAll({}).then(function(dbgame) {
+      res.json(dbgame);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/games", function(req, res) {
+    db.Games.create(req.body).then(function(dbgame) {
+      res.json(dbgame);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/games/:id", function(req, res) {
+    db.Games.destroy({ where: { id: req.params.id } }).then(function(dbgame) {
+      res.json(dbgame);
     });
   });
 };
